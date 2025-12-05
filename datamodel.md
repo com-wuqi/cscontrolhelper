40408b9a2479e0b44f553724be8df0eed0df3302
com-wuqi <wuqi9277@outlook.com>
2025-12-05 21:22:38 +0800

    init 0

---
+++ datamodel.md
@@ -0,0 +1,28 @@
+# data_model 文档
+
+## user_data 玩家
+id: int = Field(default=None, primary_key=True)
+
+student_id: int = Field(index=True, nullable=False)
+
+is_alive: bool = Field(default=True,nullable=False) # 是否活着
+
+secret_key: str = Field(index=False) # 登录凭证,由后端生成
+
+password: str = Field(index=False,nullable=False)
+
+kill: List[int] = Field(
+        default_factory=list,
+        sa_column=Column(VARCHAR)
+) # 击杀统计,内容为 student_id
+
+team: str = Field(index=True) # 队伍,由后端分配
+
+## admin_data 管理员
+id: int = Field(default=None, primary_key=True)
+
+user: str: = Field(index=True, nullable=False)
+
+password: str = Field(index=False,nullable=False)
+
+secret_key: str = Field(index=False) # 登录凭证,由后端生成
--- dependencies/datamodel.py
+++ dependencies/datamodel.py
@@ -2,7 +2,7 @@ from typing import List
 from datetime import datetime,timezone
 
 
-from sqlmodel import Field, SQLModel,Relationship,JSON,Column
+from sqlmodel import Field, SQLModel,Relationship,JSON,Column,VARCHAR
 
 """
 数据库模型
@@ -15,7 +15,7 @@ class UserBase(SQLModel):
     password: str = Field(index=True)
     # 永远不要存储用户的明文密码，也不要在响应中发送密码。
     password_salt: str = Field(index=True, nullable=False)
-    is_active: bool = Field(default=False)
+    is_active: bool = Field(default=True)
     # 是否存活
     last_login: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), nullable=False)
     secret_key: str = Field(index=False)
@@ -25,6 +25,10 @@ class UserBase(SQLModel):
 
 class User(UserBase, table=True):
     __tablename__ = "user"
+    kill: List[int] = Field(
+        default_factory=list,
+        sa_column=Column(VARCHAR)
+    )
 
 
 class AdminUser(UserBase, table=True):
